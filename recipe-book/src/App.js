394dd064e9a023d5f3933c6a5a39dfb0c27f86f8
jonwretlind <47186515+jonwretlind@users.noreply.github.com>
2022-05-26 18:54:01 -0600

    added axios

--- recipe-book/src/App.js
+++ recipe-book/src/App.js
@@ -3,20 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="RecipeBook" className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Recipe Book</h1>
+        <div className="container"></div>
       </header>
     </div>
   );
